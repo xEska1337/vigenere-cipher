9122c874ee0ef0dc238902641f2b553c9d8184bb
xEska <44504000+xEska1337@users.noreply.github.com>
2025-01-17 12:30:55 +0100

    sprawdzanie poprawności argumentów wejściowych

--- main.cpp
+++ main.cpp
@@ -18,12 +18,17 @@ int main(int argc, char **argv)
                   << "-i plik tekstowy wejściowy" << std::endl
                   << "-o plik tekstowy wyjściowy" << std::endl
                   << "-k plik tekstowy z kluczem" << std::endl;
+        exit(0);
     }
     for (int i = 1; i < argc; i++)
     {
         if (std::string(argv[i]) == "--en")
         {
             encrypt = true;
+        }
+        else if (std::string(argv[i]) == "--de")
+        {
+            encrypt = false;
         }
         else if (std::string(argv[i]) == "--br")
         {
@@ -35,24 +40,78 @@ int main(int argc, char **argv)
         }
         else if (std::string(argv[i]) == "-i")
         {
-            inpFile = argv[++i];
+            if (i + 1 < argc)
+            {
+                inpFile = argv[++i];
+            }
+            else
+            {
+                std::cout << "brak nazwy pliku wejściowego";
+                exit(0);
+            }
         }
         else if (std::string(argv[i]) == "-o")
         {
-            outFile = argv[++i];
+            if (i + 1 < argc)
+            {
+                outFile = argv[++i];
+            }
+            else
+            {
+                std::cout << "brak nazwy pliku wyjściowego";
+                exit(0);
+            }
         }
         else if (std::string(argv[i]) == "-k")
         {
-            keyFile = argv[++i];
+            if (i + 1 < argc)
+            {
+                keyFile = argv[++i];
+            }
+            else
+            {
+                std::cout << "brak nazwy pliku z kluczem";
+                exit(0);
+            }
         }
+        else
+        {
+            std::cout << "nieznany parametr \"" << argv[i] << "\"";
+            exit(0);
+        }
+    }
+
+    if (inpFile.empty())
+    {
+        std::cout << "brak pliku wejsciowego";
+        exit(0);
+    }
+    if (outFile.empty())
+    {
+        std::cout << "brak pliku wyjsciowego";
+        exit(0);
     }
 
     if (crack)
     {
+        std::cout << "operacja łamania szyfru z pliku " << inpFile << " do pliku " << outFile << std::endl;
         cracker(inpFile, outFile, polskie);
     }
     else
     {
+        if (keyFile.empty())
+        {
+            std::cout << "brak pliku klucza";
+            exit(0);
+        }
+        if (encrypt)
+        {
+            std::cout << "operacja szyfrowania z pliku " << inpFile << " do pliku " << outFile << " z kluczem " << keyFile << std::endl;
+        }
+        else
+        {
+            std::cout << "operacja deszyfrowania z pliku " << inpFile << " do pliku " << outFile << " z kluczem " << keyFile << std::endl;
+        }
         cryptonator(inpFile, outFile, keyFile, encrypt, polskie);
     }
 
